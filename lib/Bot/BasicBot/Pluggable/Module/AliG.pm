42574cf77413018cbde00134e6ffdcfc22632bf1
William Wolf <wwolf@cpan.org>
2011-10-23 02:46:25 +0000

    Import of WWOLF/Bot-BasicBot-Pluggable-Module-AliG-0.0001 from CPAN.
    gitpan-cpan-distribution: Bot-BasicBot-Pluggable-Module-AliG
    gitpan-cpan-version:      0.0001
    gitpan-cpan-path:         WWOLF/Bot-BasicBot-Pluggable-Module-AliG-0.0001.tar.gz
    gitpan-cpan-author:       WWOLF
    gitpan-cpan-maturity:     released

--- lib/Bot/BasicBot/Pluggable/Module/AliG.pm
+++ lib/Bot/BasicBot/Pluggable/Module/AliG.pm
@@ -2,9 +2,10 @@ use strict;
 use warnings;
 package Bot::BasicBot::Pluggable::Module::AliG;
 use base qw(Bot::BasicBot::Pluggable::Module);
-use Acme::AliG;
 
-# ABSTRACT: turns phrases into alig-phrases
+our $VERSION = '0.0001'; # VERSION
+
+use Acme::AliG;
 
 sub help { return "Talk like Ali G."; }
 
@@ -16,4 +17,41 @@ sub said {
     return Acme::AliG::alig($phrase) if(lc($command) eq 'alig');
 }
 
+# ABSTRACT: IRC bot that translates phrases from English to Ali G
+
+
 1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Bot::BasicBot::Pluggable::Module::AliG - IRC bot that translates phrases from English to Ali G
+
+=head1 VERSION
+
+version 0.0001
+
+=head1 SYNOPSIS
+
+    use Bot::BasicBot::Pluggable::Module;
+
+=head1 DESCRIPTION
+
+This is an IRC bot that translates phrases from English to Ali G.
+The keyword alig triggers the translation.
+
+=head1 AUTHOR
+
+William Wolf <throughnothing@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by William Wolf.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
